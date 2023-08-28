3a0131d49781d960752dec8c66a99b1277c4ea88
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-08-28 17:42:15 +0800

    Added global versioning and validation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -12,6 +13,27 @@ async function bootstrap() {
     new FastifyAdapter(),
     { cors: true },
   );
+
+  // Append api prefix to your base url
+  app.setGlobalPrefix('api');
+  // Enable versioning on this api
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  // Enable body validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Remove and return error if unwanted properties exists
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+      // Automatically transform payloads to be objects typed according to their DTO classes
+      transform: true,
+    }),
+  );
+
   await app.listen(3001);
 }
 bootstrap();
