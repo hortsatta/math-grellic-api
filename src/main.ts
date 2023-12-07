711715cc290f4543b8aba6d5816ccb60409eed10
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-12-07 22:58:52 +0800

    Update fastify cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -10,16 +11,33 @@ import { AppModule } from './modules/app.module';
 import { DatabaseExceptionFilter } from './common/filters/database-exception.filter';
 import { AuthSocketAdapter } from './common/adapters/auth-socket.adapter';
 
+dotenv.config();
+
 async function bootstrap() {
+  const adapter = new FastifyAdapter();
+  // Enable cors with options
+  adapter.enableCors({
+    origin: JSON.parse(process.env.CORS_ORIGINS),
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    credentials: true,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    adapter,
   );
 
   // Get config service to access env variables
   const configService = app.get<ConfigService>(ConfigService);
-  // Enable and assign origins
-  app.enableCors();
 
   // Append api prefix to your base url
   app.setGlobalPrefix('api');
