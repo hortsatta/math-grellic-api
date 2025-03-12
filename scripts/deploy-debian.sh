e0b3e267cd1687a157766f1135a54518bad1d08f
Erwin Errol Calanuga <decontrrol@proton.me>
2025-03-12 14:41:00 +0800

    Move docker scripts and add script for deploy

---
+++ scripts/deploy-debian.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ~/master/repo/math-grellic/math-grellic-api
+
+# Pull the latest changes from GitHub
+git pull origin master 
+
+# Stop and remove old containers
+docker compose down
+
+# Pull latest images if needed
+docker compose pull
+
+# Rebuild and start the updated containers
+docker compose -f scripts/docker-compose.yml up -d
