c08c230e77d4c28e86881c5934392ffcf7016b74
Erwin Errol Calanuga <decontrrol@proton.me>
2025-04-06 00:24:52 +0800

    Temporary sync db

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -78,7 +78,7 @@ import { AuditLog } from './audit-log/entities/audit-log.entity';
             Announcement,
             AuditLog,
           ],
-          synchronize: process.env.NODE_ENV !== 'production',
+          synchronize: true,
           ssl,
         };
       },
